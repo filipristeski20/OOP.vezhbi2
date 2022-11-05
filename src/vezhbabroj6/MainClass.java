72ef46a85285ee10ceed4e8fc5477f35d6bd0202
Acer <Acer@DESKTOP-UF5FERV.Home>
2022-11-05 20:19:39 +0100

    commit 4

---
+++ src/vezhbabroj6/MainClass.java
@@ -0,0 +1,9 @@
+package vezhbabroj6;
+
+public class MainClass {
+	public static void main(String[] args) {
+		Proizvod hrana = new Proizvod();
+		System.out.println("Cenata na " +hrana.ime + " so tezhina od " + hrana.tezhina + "g iznesuva " + hrana.cena);
+	}
+
+}
---
+++ src/vezhbabroj6/Proizvod.java
@@ -0,0 +1,17 @@
+package vezhbabroj6;
+
+public class Proizvod {
+	
+	public String ime;
+	public int tezhina;
+	public int cena;
+	
+	public Proizvod(){
+		
+		this.ime = "Chokoladoto";
+		this.tezhina = 100;
+		this.cena = 55;		
+		
+	}
+
+}
